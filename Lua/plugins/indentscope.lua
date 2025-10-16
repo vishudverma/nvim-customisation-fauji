8c428d4f2ad1718e05ed9041dab6b86bb76373b4
vishudverma <244vishudverma@gmail.com>
2025-10-17 02:29:49 +0530

    corrected indentation added sql capacity

--- Lua/plugins/indentscope.lua
+++ Lua/plugins/indentscope.lua
@@ -1,21 +1,23 @@
 return {
-  "echasnovski/mini.indentscope",
-  version = false,
-  event = "VeryLazy",
-  opts = {
-    symbol = "│",
-    options = { try_as_border = true },
-  },
-   init = function()
-    vim.api.nvim_create_autocmd("FileType", {
-      pattern = {
-        "alpha",
-        "dashboard",
-        "starter",
-      },
-      callback = function()
-        vim.b.miniindentscope_disable = true
-      end,
-    })
-  end,
+	"echasnovski/mini.indentscope",
+	version = false,
+	event = "VeryLazy",
+	opts = {
+		symbol = "│",
+		options = { try_as_border = true },
+	}
+	,
+	init = function()
+		vim.api.nvim_create_autocmd("FileType",{
+			pattern ={
+				"alpha",
+				"dashboard",
+				"starter",
+			}
+			,
+			callback = function()
+				vim.b.miniindentscope_disable = true
+			end,
+		})
+	end,
 }
