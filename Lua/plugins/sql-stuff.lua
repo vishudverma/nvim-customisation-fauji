8e22484e0c02f18b1dd63e029a71258aba6ce34c
vishudverma <244vishudverma@gmail.com>
2025-10-25 13:31:15 +0530

    improved sql functionality

--- Lua/plugins/sql-stuff.lua
+++ Lua/plugins/sql-stuff.lua
@@ -14,7 +14,7 @@ return {
     init = function()
       -- Your DBUI configuration
       vim.g.db_ui_use_nerd_fonts = 1
-	  vim.keymap.set('n', '<C-s>', ':DBUIToggle<CR>', {})
+	  vim.keymap.set('n', '<leader>st', ':DBUIToggle<CR>', {})
     end,
   },
 }
